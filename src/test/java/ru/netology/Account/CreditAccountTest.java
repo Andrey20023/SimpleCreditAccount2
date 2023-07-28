26da921e47dbbc8505cd6f3c50c44c403be6f127
Andrey <andreiperevertov@yandex.ru>
2023-07-28 22:41:04 +0300

    Add

--- src/test/java/ru/netology/Account/CreditAccountTest.java
+++ src/test/java/ru/netology/Account/CreditAccountTest.java
@@ -1,29 +1,41 @@
 package ru.netology.Account;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreditAccountTest {
+    CreditAccount creditAccount;
 
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("BeforeEach");
+        creditAccount = new CreditAccount(-100, -500);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("AfterEach");
+        creditAccount = null;
+    }
 
     @org.junit.jupiter.api.Test
     public void add() {
-        CreditAccount creditAccount = new CreditAccount(-100, -500);
-        assertEquals(false, creditAccount.add(500));
         assertFalse(creditAccount.add(200));
         assertTrue(creditAccount.add(-50));
     }
 
-    @org.junit.jupiter.api.Test
-    void pay() {
-        CreditAccount creditAccount = new CreditAccount(-100, -500);
-        assertEquals(true, creditAccount.pay(400));
-        assertFalse(creditAccount.pay(200));
-        assertTrue(creditAccount.pay(-50));
+    @ParameterizedTest
+    @ValueSource(longs = {100, 200, 300})
+    void pay(long argument) {
+        assertEquals(true, creditAccount.pay(argument));
     }
 
     @org.junit.jupiter.api.Test
     public void result() {
-        CreditAccount creditAccount = new CreditAccount(-100, -500);
         long actual = creditAccount.result(-200);
         long expected = -300;
         assertEquals(expected, actual);
